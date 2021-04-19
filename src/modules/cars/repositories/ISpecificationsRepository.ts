1b8dd8451dc01e6211233931dae3858f44a04849
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2021-04-19 14:46:57 -0300

    chore: add migration specifications

--- src/modules/cars/repositories/ISpecificationsRepository.ts
+++ src/modules/cars/repositories/ISpecificationsRepository.ts
@@ -6,8 +6,8 @@ interface ICreateSpecificationDTO {
 }
 
 interface ISpecificationsRepository {
-  findByName(name: string): Specification;
-  create({ name, description }: ICreateSpecificationDTO): void;
+  findByName(name: string): Promise<Specification>;
+  create({ name, description }: ICreateSpecificationDTO): Promise<void>;
 }
 
 export { ISpecificationsRepository, ICreateSpecificationDTO };
