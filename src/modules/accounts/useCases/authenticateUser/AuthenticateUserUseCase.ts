e827d9610934daf2c7a86df447cff9439a0e51c2
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2021-05-02 11:58:42 -0300

    chore created usecase for creating sessions

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,56 @@
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { inject } from 'tsyringe';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+class AuthenticateUserUseCase {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const token = sign({}, 'c8b96b3ff08e5f11fa48b9c8d26fc9f8', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { AuthenticateUserUseCase };
