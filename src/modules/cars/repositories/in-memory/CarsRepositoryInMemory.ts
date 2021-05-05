b4c3f7ea93aab359bffd583b6a7dc957ab71e4b7
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2021-05-05 14:56:10 -0300

    chore: use case list cars

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -12,6 +12,27 @@ class CarsRepositoryInMemory implements ICarsRepository {
     return car;
   }
 
+  async findAvailable(
+    brand?: string,
+    category_id?: string,
+    name?: string,
+  ): Promise<Car[]> {
+    const all = this.cars.filter(car => {
+      if (
+        car.available === true &&
+        ((brand && car.brand === brand) ||
+          (category_id && car.category_id === category_id) ||
+          (name && car.name === name))
+      ) {
+        return car;
+      }
+
+      return null;
+    });
+
+    return all;
+  }
+
   async create({
     name,
     description,
