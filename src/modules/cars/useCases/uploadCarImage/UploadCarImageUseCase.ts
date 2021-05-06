d892e397258dd824c4e83b2103c984ce36697610
Robson José Lopes Gonçalves Júnior <robsonlopes.jr@gmail.com>
2021-05-06 10:38:19 -0300

    chore: add use case cars_images

---
+++ src/modules/cars/useCases/uploadCarImage/UploadCarImageUseCase.ts
@@ -0,0 +1,25 @@
+import { inject, injectable } from 'tsyringe';
+
+import { CarImage } from '@modules/cars/infra/typeorm/entities/CarImage';
+import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository';
+
+interface IRequest {
+  car_id: string;
+  images_name: string[];
+}
+
+@injectable()
+class UploadCarImageUseCase {
+  constructor(
+    @inject('CarsImagesRepository')
+    private carsImagesRepository: ICarsImagesRepository,
+  ) {}
+
+  async execute({ car_id, images_name }: IRequest): Promise<void> {
+    images_name.map(async image => {
+      await this.carsImagesRepository.create(car_id, image);
+    });
+  }
+}
+
+export { UploadCarImageUseCase };
